990276d72d55e6f9b41524af570a5fa12d05c435
Nemolite <g25092011@mail.ru>
2017-10-06 15:36:08 +0300

    config_admin

--- engine/core/config/config.php
+++ engine/core/config/config.php
@@ -10,14 +10,40 @@ namespace engine\core\config;
 
 class config {
 
-    public  static function item($key,$group = 'itmes')
+    public  static function item($key,$group = 'main')
     {
+        $groupItems = static::file($group);
 
+        return isset($groupItems[$key])? $groupItems[$key] : null;
     }
 
     public  static function file($group)
     {
+$path = $_REQUEST['DOCUMENT_ROOT']. '/'.mb_strtolower(ENV). '/Config/'.$group. '.php';
 
+        if (file_exists($path))
+        {
+            $items = require_once $path;
+
+            if(is_array($items))
+            {
+                return $items;
+            }
+            else
+            {
+                throw new \Exception(
+                    sprintf('Config file <strong>%s</strong> is not a valid array',$path)
+                );
+            }
+        }
+        else
+        {
+            throw new \Exception(
+                sprintf('Connot load config from file, file<strong>%s</strong> does not exist.',$path)
+
+            );
+        }
+        return false;
     }
 
 
